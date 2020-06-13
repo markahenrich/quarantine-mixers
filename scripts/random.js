ab29ad3eb46fd156128355ed669145dfb1906939
markahenrich <mark.henrich@icloud.com>
2020-06-13 16:47:36 -0700

    finished jquery HTML insertions

--- scripts/random.js
+++ scripts/random.js
@@ -7,8 +7,42 @@ async function getRandom() {
 
 $(document).ready(async function() {
     let drink = await getRandom();
+    let { 
+        strIngredient1, strIngredient2, strIngredient3, strIngredient4,
+        strIngredient5, strMeasure1, strMeasure2, strMeasure3, strMeasure4, 
+        strMeasure5 
+    } = drink;
+    let ingredients = [strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5];
+    let measures = [strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5]
     
     $('.main-random').append(`
     <img src='${drink.strDrinkThumb}'/>
-    <h1>${drink.strDrink}</h1>`);
+    <h1>${drink.strDrink}</h1>
+    <ul class='list'></ul>
+    <br></br>
+    <p>${drink.strInstructions}</p>
+    `);
+
+    let counter = 0;
+    
+    ingredients.forEach(ingredient => {
+
+        if (ingredient != null) {
+            
+            if (measures[counter] != null) {
+                $('.list').append(`
+                    <li>${measures[counter] + ' ' + ingredient}
+                `)
+            }
+            
+            else {
+                $('.list').append(`
+                <li>${ingredient}</li>
+            `) 
+            }
+        }
+
+        counter += 1;
+
+    })
 });
