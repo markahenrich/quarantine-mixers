9ce3245f655781dd43804988ce6b6f92c617e9cf
markahenrich <mark.henrich@icloud.com>
2020-06-14 18:24:02 -0700

    added content to main page

--- scripts/home.js
+++ scripts/home.js
@@ -10,11 +10,20 @@ $(document).ready(function() {
         <br></br>
         <h2>Welcome</h2>
         <img src='https://cdn.pixabay.com/photo/2013/02/21/19/06/beach-84533_1280.jpg'/>
-        <p>Don't let quarantine get you down!</p>
-        <p>Get started by clicking Categories at the top or press Random if you're feeling lucky.</p> 
+        <p>Don't let quarantine get you down! Get started by clicking</p>
+        <p>Categories at the top or press Random if you're feeling lucky.</p> 
         <br></br>
         <h2>Be safe!</h2>
-        <p>Follow quarantine guidelines, wash your hands, and wear a mask!</p>
-        <p>Get more information <a href='https://www.cdc.gov/coronavirus/2019-ncov/index.html' target="_blank">here.</a></p>
+        <p>Follow quarantine guidelines, wash your hands,</p>
+        <p>and wear a mask! Get more information <a href='https://www.cdc.gov/coronavirus/2019-ncov/index.html' target="_blank">here.</a></p>
+        <br></br>
+        <h2>Don't over do it!</h2>
+        <p>Drink responsibly. If you believe you have a problem,</p>
+        <p>consider looking into the resources below to find help.</p>
+        <p></p>
+        <ul>
+            <li><a href='https://www.alcohol.org' target='_blank'>alcohol.org</a></li>
+            <li><a href='https://www.samhsa.gov/find-help/national-helpline' target='_blank'>Substance Abuse and Mental Health Services Administration</a></li>
+        </ul>
     `);
 });
